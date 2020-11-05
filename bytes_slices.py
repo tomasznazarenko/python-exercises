1c170feabb70c11614a4a2df938cd5dbed4ed2dc
Tomasz Nazarenko <tomasz.nazarenko@icloud.com>
2020-11-05 03:09:31 +0100

    Update bytes_slices.py

--- bytes_slices.py
+++ bytes_slices.py
@@ -1,4 +1,4 @@
 # Write a code that converts the given string to a bytes object and outputs its last item. 
 
 text_in_bytes = input().encode('utf-8')
-print(text_to_bytes[-1])
+print(text_in_bytes[-1])
